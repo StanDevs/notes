3da9bf29ecfa75ef8b118bb70f42e9c9568a15db
Stan Devs <stanl.devs@gmail.com>
2021-04-13 22:54:05 +0200

    functional component (without state change)

--- src/Components/Note.js
+++ src/Components/Note.js
@@ -1,17 +1,9 @@
 import React, { Component } from "react";
 
-class Note extends Component {
-  render() {
-    // const note = this.props.note;
-    // const onDelete = this.props.onDelete;
-    const { note, onDelete } = this.props;
-    return (
-      <li key={note.id}>
-        {note.title}
-        <button onClick={() => onDelete(note.id)}>X</button>
-      </li>
-    );
-  }
-}
-
+const Note = ({ note, onDelete }) => (
+  <li key={note.id}>
+    {note.title}
+    <button onClick={() => onDelete(note.id)}>X</button>
+  </li>
+);
 export default Note;
